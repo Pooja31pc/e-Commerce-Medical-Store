c6b08f13c395b002a7e8b130855be457832a11a6
Pooja31pc <pooja31pc@gmail.com>
2021-01-24 09:47:30 +0530

    modified user table

--- src/main/java/com/meddeli/onlinemedicalstore/model/User.java
+++ src/main/java/com/meddeli/onlinemedicalstore/model/User.java
@@ -3,34 +3,21 @@ package com.meddeli.onlinemedicalstore.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name="tbl_user")
+//@Table(name="tbl_user")
 public class User {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
-
-    private String email;
-
-    @Column(name="user_name")
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
     private String username;
-
     private String password;
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getUsername() {
@@ -49,14 +36,76 @@ public class User {
         this.password = password;
     }
 
-    public User(Long id, String email, String username, String password) {
+    public User() {
+    }
+
+    public User(int id, String username, String password) {
         this.id = id;
-        this.email = email;
         this.username = username;
         this.password = password;
     }
 
-    public User() {
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
+
+    //    @Id
+//    @GeneratedValue(strategy= GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String email;
+//
+//    @Column(name="user_name")
+//    private String username;
+//
+//    private String password;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public User(Long id, String email, String username, String password) {
+//        this.id = id;
+//        this.email = email;
+//        this.username = username;
+//        this.password = password;
+//    }
+//
+//    public User() {
+//    }
+//
+//    public User(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//    }
 
 }
